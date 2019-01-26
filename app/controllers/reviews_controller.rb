142e21465f4200786cb83227682d79e93a8d6022
Xin <Detective_boys@hotmil.com>
2019-01-26 14:33:26 +0800

    add review button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,3 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
 
-  def new
-    @movie = Movie.find(params[:movie_id])
-    @review = Review.new
-  end
-
-  def create
-    @movie = Movie.find(params[:movie_id])
-    @review = Review.new(review_params)
-    @review.user = current_user
-    @review.movie = @movie
-    
-    if @review.save
-      redirect_to movie_path(movie)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def review_params
-    params.require(:review).permit(:content)
-  end
 end
