760ad2f4a2990a9bb9209791f50a0e4456e048f9
Xin <Detective_boys@hotmil.com>
2019-01-26 20:29:46 +0800

    implement reviews#edit/update/destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,13 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
+  before_action :find_review_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @movie = Movie.find(params[:movie_id])
     @review = Review.new
+  end
+
+  def edit
   end
 
   def create
@@ -19,7 +23,29 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def update
+    if @review.update(review_params)
+      redirect_to movie_path(@movie), notice:"Review Updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+      redirect_to movie_path(@movie), alert: "Review Deleted"
+  end
+
   private
+
+  def find_review_and_check_permission
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+
+    if current_user != @review.user
+      redirect_to root_path, alert:"You Have No Permission"
+    end
+  end
 
   def review_params
     params.require(:review).permit(:content)
